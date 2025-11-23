ba8691d612582e3fb6f827badcc19ab251c2adf2
Syed Shifat <shifat1755@gmail.com>
2025-11-24 00:17:07 +0600

    feat(frontend):added login and registration pages and functionalities

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,5 @@
+function home() {
+  return <div>Home Page</div>;
+}
+
+export default home;
---
+++ frontend/src/service/authService.ts
@@ -0,0 +1,23 @@
+import api from "./api";
+
+
+export function login(email: string, password: string) {
+    return api.post("/auth/login", { email, password })
+        .then(res => res.data)
+        .catch((err: any) => {
+            if (err.response?.status === 401) {
+                throw new Error("Wrong email or password");
+            }
+            throw new Error("Something went wrong. Please try again later.");
+        });
+}
+
+export async function register(data: any) {
+    const response = await api.post("/auth/signup", data);
+    return response.data;
+}
+export async function logout() {
+    const response = await api.post("/auth/logout");
+    localStorage.removeItem("authToken");//later in feed page we will do it in proper way
+    return response.data;
+}
