c9a3e5b88ef175ab203e2450267d2b6265d63e8e
Syed Shifat <shifat1755@gmail.com>
2025-11-24 00:17:07 +0600

    feat(frontend):added login and registration pages and functionalities

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,190 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { login } from "../service/authService";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(true);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    login(email, password)
+      .then((data) => {
+        localStorage.setItem("authToken", data.token);
+        navigate("/feed");
+      })
+      .catch((err: any) => {
+        console.error("Login error:", err);
+        alert(err.message);
+      });
+  };
+
+  const handleGoogleLogin = () => {
+    console.log("Google login");
+  };
+
+  return (
+    <section className="_social_login_wrapper _layout_main_wrapper">
+      <div className="_shape_one">
+        <img src="/assets/images/shape1.svg" alt="" className="_shape_img" />
+        <img
+          src="/assets/images/dark_shape.svg"
+          alt=""
+          className="_dark_shape"
+        />
+      </div>
+      <div className="_shape_two">
+        <img src="/assets/images/shape2.svg" alt="" className="_shape_img" />
+        <img
+          src="/assets/images/dark_shape1.svg"
+          alt=""
+          className="_dark_shape _dark_shape_opacity"
+        />
+      </div>
+      <div className="_shape_three">
+        <img src="/assets/images/shape3.svg" alt="" className="_shape_img" />
+        <img
+          src="/assets/images/dark_shape2.svg"
+          alt=""
+          className="_dark_shape _dark_shape_opacity"
+        />
+      </div>
+      <div className="_social_login_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_login_left">
+                <div className="_social_login_left_image">
+                  <img
+                    src="/assets/images/login.png"
+                    alt="Image"
+                    className="_left_img"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_login_content">
+                <div className="_social_login_left_logo _mar_b28">
+                  <img
+                    src="/assets/images/logo.svg"
+                    alt="Image"
+                    className="_left_logo"
+                  />
+                </div>
+                <p className="_social_login_content_para _mar_b8">
+                  Welcome back
+                </p>
+                <h4 className="_social_login_content_title _titl4 _mar_b50">
+                  Login to your account
+                </h4>
+                <button
+                  type="button"
+                  className="_social_login_content_btn _mar_b40"
+                  onClick={handleGoogleLogin}
+                >
+                  <img
+                    src="/assets/images/google.svg"
+                    alt="Image"
+                    className="_google_img"
+                  />
+                  <span>Or sign-in with google</span>
+                </button>
+                <div className="_social_login_content_bottom_txt _mar_b40">
+                  <span>Or</span>
+                </div>
+                <form className="_social_login_form" onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control _social_login_input"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label className="_social_login_label _mar_b8">
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control _social_login_input"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="form-check _social_login_form_check">
+                        <input
+                          className="form-check-input _social_login_form_check_input"
+                          type="checkbox"
+                          name="rememberMe"
+                          id="rememberMe"
+                          checked={rememberMe}
+                          onChange={(e) => setRememberMe(e.target.checked)}
+                        />
+                        <label
+                          className="form-check-label _social_login_form_check_label"
+                          htmlFor="rememberMe"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-12">
+                      <div className="_social_login_form_left">
+                        <p className="_social_login_form_left_para">
+                          Forgot password?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12 col-xl-12 col-sm-12">
+                      <div className="_social_login_form_btn _mar_t40 _mar_b60">
+                        <button
+                          type="submit"
+                          className="_social_login_form_btn_link _btn1"
+                        >
+                          Login now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <div className="row">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="_social_login_bottom_txt">
+                      <p className="_social_login_bottom_txt_para">
+                        Dont have an account?{" "}
+                        <Link to="/register">Create New Account</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
---
+++ frontend/src/pages/Registration.tsx
@@ -0,0 +1,245 @@
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { register } from "../service/authService";
+import { useNavigate } from "react-router-dom";
+
+function Registration() {
+  const [email, setEmail] = useState("");
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [agreeToTerms, setAgreeToTerms] = useState(true);
+  const Navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== repeatPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+    if (password.length < 5) {
+      alert("Password must be at least 5 characters long!");
+      return;
+    }
+    register({ email, first_name, last_name, password })
+      .then((data) => {
+        console.log("Registration successful:", data);
+        Navigate("/login");
+      })
+      .catch((err: any) => {
+        console.error("Registration error:", err);
+        alert(err.message);
+      });
+    console.log("Registration:", { email, password, agreeToTerms });
+  };
+
+  const handleGoogleRegistration = () => {
+    // Handle Google registration logic here
+    console.log("Google registration");
+  };
+
+  return (
+    <section className="_social_registration_wrapper _layout_main_wrapper">
+      <div className="_shape_one">
+        <img src="/assets/images/shape1.svg" alt="" className="_shape_img" />
+        <img
+          src="/assets/images/dark_shape.svg"
+          alt=""
+          className="_dark_shape"
+        />
+      </div>
+      <div className="_shape_two">
+        <img src="/assets/images/shape2.svg" alt="" className="_shape_img" />
+        <img
+          src="/assets/images/dark_shape1.svg"
+          alt=""
+          className="_dark_shape _dark_shape_opacity"
+        />
+      </div>
+      <div className="_shape_three">
+        <img src="/assets/images/shape3.svg" alt="" className="_shape_img" />
+        <img
+          src="/assets/images/dark_shape2.svg"
+          alt=""
+          className="_dark_shape _dark_shape_opacity"
+        />
+      </div>
+      <div className="_social_registration_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_registration_right">
+                <div className="_social_registration_right_image">
+                  <img src="/assets/images/registration.png" alt="Image" />
+                </div>
+                <div className="_social_registration_right_image_dark">
+                  <img src="/assets/images/registration1.png" alt="Image" />
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_registration_content">
+                <div className="_social_registration_right_logo _mar_b28">
+                  <img
+                    src="/assets/images/logo.svg"
+                    alt="Image"
+                    className="_right_logo"
+                  />
+                </div>
+                <p className="_social_registration_content_para _mar_b8">
+                  Get Started Now
+                </p>
+                <h4 className="_social_registration_content_title _titl4 _mar_b50">
+                  Registration
+                </h4>
+                <button
+                  type="button"
+                  className="_social_registration_content_btn _mar_b40"
+                  onClick={handleGoogleRegistration}
+                >
+                  <img
+                    src="/assets/images/google.svg"
+                    alt="Image"
+                    className="_google_img"
+                  />
+                  <span>Register with google</span>
+                </button>
+                <div className="_social_registration_content_bottom_txt _mar_b40">
+                  <span>Or</span>
+                </div>
+                <form
+                  className="_social_registration_form"
+                  onSubmit={handleSubmit}
+                >
+                  <div className="row">
+                    {/* First Name */}
+                    <div className="col-md-6 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">
+                          First Name
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control _social_registration_input"
+                          value={first_name}
+                          onChange={(e) => setFirst_name(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    {/* Last Name */}
+                    <div className="col-md-6 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">
+                          Last Name
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control _social_registration_input"
+                          value={last_name}
+                          onChange={(e) => setLast_name(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control _social_registration_input"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control _social_registration_input"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                          pattern="(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}"
+                          title="Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, one number, and one special character"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                      <div className="_social_registration_form_input _mar_b14">
+                        <label className="_social_registration_label _mar_b8">
+                          Repeat Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control _social_registration_input"
+                          value={repeatPassword}
+                          onChange={(e) => setRepeatPassword(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-xl-12 col-md-12 col-sm-12">
+                      <div className="form-check _social_registration_form_check">
+                        <input
+                          className="form-check-input _social_registration_form_check_input"
+                          type="checkbox"
+                          name="agreeToTerms"
+                          id="agreeToTerms"
+                          checked={agreeToTerms}
+                          onChange={(e) => setAgreeToTerms(e.target.checked)}
+                        />
+                        <label
+                          className="form-check-label _social_registration_form_check_label"
+                          htmlFor="agreeToTerms"
+                        >
+                          I agree to terms & conditions
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12 col-xl-12 col-sm-12">
+                      <div className="_social_registration_form_btn _mar_t40 _mar_b60">
+                        <button
+                          type="submit"
+                          className="_social_registration_form_btn_link _btn1"
+                        >
+                          Register now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <div className="row">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div className="_social_registration_bottom_txt">
+                      <p className="_social_registration_bottom_txt_para">
+                        Already have an account? <Link to="/login">Login</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Registration;
